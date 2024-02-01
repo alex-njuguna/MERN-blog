3f6b7ac66701de08f6dd920e6419f411d0b4825e
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-02 02:13:56 +0300

    commenting on the server

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
+// npm install express
+// npm install nodemon
+// change package.json to allow for node running indexe.js("type": "module")
+// adjust scripts to allow for running nodemon in dev and node in production
+// "dev": "nodemon api/index.js", - npm run dev
+// "start": "node api/index.js"   - npm run start
+
 import express from "express";
 
 const app = express();
