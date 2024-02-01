d650f39714d5c9d76b14db5ae536337a5151970a
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-01 04:04:52 +0300

    pages setup and routing

---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Dashboard from "./Dashboard";
+import Home from "./Home";
+import Projects from "./Projects";
+import SignIn from "./Signin";
+import SignUp from "./Signup";
+
+export { About, Dashboard, Home, Projects, SignIn, SignUp };
