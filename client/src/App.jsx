dfb6c0cf38e4a18ff5829ce724661c3024403e07
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-01 04:16:49 +0300

    default not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { About, Home, Dashboard, Projects, SignIn, SignUp } from "./pages";
+import NotFound from "./NotFound";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="projects/" element={<Projects />} />
           <Route path="sign-in/" element={<SignIn />} />
           <Route path="sign-up/" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/NotFound.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="alert alert-warning">
+      <div className="container d-flex flex-column min-vh-100 align-items-center justify-content-center">
+        <h1 className="mb-5">404 Error:</h1>
+        <h2>Page Not Found</h2>
+      </div>
+    </div>
+  );
+}
